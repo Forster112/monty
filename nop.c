3b4593dcc0dae3861fed4bed4b53bf328d901893
Forster112 <forstermichael112@gmail.com>
2022-06-19 14:41:44 -0700

    i want all

--- nop.c
+++ nop.c
@@ -1,13 +1,27 @@
 #include "monty.h"
 
 /**
- * nop - opcode that does nothing
+ * d_stack - opcode that does nothing
  * @stack: pointer to stack
  * @ln: line number
  * Return: nothing
  */
-void nop(stack_t **stack, unsigned int ln)
+void d_stack(stack_t **stack, unsigned int ln)
 {
 	(void)stack;
 	(void)ln;
+	bus.lifi = 0;
+}
+
+/**
+ * d_queue - opcode
+ * @stack: pointer to stack
+ * @ln: line number
+ * Return: nothing
+ */
+void d_queue(stack_t **stack, unsigned int ln)
+{
+	(void)stack;
+	(void)ln;
+	bus.lifi = 1;
 }
