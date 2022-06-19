bbaeaa12a1fd8fc484652e86a4a97d45e4f1f069
Forster112 <91550261+Forster112@users.noreply.github.com>
2022-06-19 23:01:32 +0100

    Update nop.c

--- nop.c
+++ nop.c
@@ -1,27 +1,13 @@
 #include "monty.h"
 
 /**
- * d_stack - opcode that does nothing
+ * nop - opcode that does nothing
  * @stack: pointer to stack
  * @ln: line number
  * Return: nothing
  */
-void d_stack(stack_t **stack, unsigned int ln)
+void nop(stack_t **stack, unsigned int ln)
 {
 	(void)stack;
 	(void)ln;
-	bus.lifi = 0;
-}
-
-/**
- * d_queue - opcode
- * @stack: pointer to stack
- * @ln: line number
- * Return: nothing
- */
-void d_queue(stack_t **stack, unsigned int ln)
-{
-	(void)stack;
-	(void)ln;
-	bus.lifi = 1;
 }
